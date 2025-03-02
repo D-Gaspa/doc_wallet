1dfd90a2903558fa2b64f755aa07026e40f612e0
GEORGINAZERON <georgina.zeronca@udlap.mx>
2025-03-02 15:46:29 -0600

    Fixed metro.config.js, it was disabled eslint for two lines as , config files are not transpiled so Node.js native syntax must be enforced
    Fixed build.gradle settings. Environment is now fixed and supported for .env.development, correct format can be seen in .env.development.example

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,18 @@
-import { getDefaultConfig, mergeConfig } from "@react-native/metro-config"
+// metro.config.js
+// metro.config.js uses Common JS-style therefore eslint import suggestions is not supported
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const { getDefaultConfig } = require("@react-native/metro-config")
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const path = require("path")
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import("@react-native/metro-config").MetroConfig}
- */
-const config = {}
+module.exports = (async () => {
+    const config = await getDefaultConfig(__dirname)
+    config.projectRoot = path.resolve(__dirname)
+    config.resolver.sourceExts = ["js", "jsx", "ts", "tsx", "json"]
+    config.resolver.useWatchman = false
+    config.reporter = {
+        update: () => {},
+    }
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config)
+    return config
+})()
