94cdfeb31cf2562e55b46ef9e0607ac41dd7da1d
Dicka Lezama <dj_lezama@hotmail.com>
2025-03-08 16:53:18 -0600

    Design System Setup with initial values (values obtained from figma, subject to change throughout the development process) :)

---
+++ src/config/theme/colors.ts
@@ -0,0 +1,17 @@
+export const lightThemeColors = {
+    background: "#FFFFFF",
+    primary: "#31859A",
+    secondary: "#DBEFF4",
+    text: "000000",
+    card: "#FAFAFA",
+    searchbar: "F8F8F8",
+}
+
+export const darkThemeColors = {
+    background: "#323232",
+    primary: "#31859A",
+    secondary: "#13434F",
+    text: "FFFFFF",
+    card: "#282828",
+    searchbar: "414141",
+}
---
+++ src/config/theme/typography.ts
@@ -0,0 +1,22 @@
+export const typography = {
+    fontFamily: {
+        medium: "Inter-Medium",
+        bold: "Inter-Bold",
+    },
+    fontSize: {
+        xl: 64, // i.e "Bienvenido a DocWallet" en la página de inicio
+        lg: 60, // i.e los títulos de páginas "Carpetas", "Documentos de viaje", etc.
+        md: 24, //i.e subtitles
+        base: 20, // i.e títulos de documentos, nombre de usuario, resultados de búsqueda, etc
+        sm: 15, // i.e "Ya tengo una cuenta" en la página de registro
+        xm: 12,
+    },
+    lineHeight: {
+        xl: 60,
+        lg: 60,
+        md: 26,
+        base: 23,
+        sm: 18,
+        xm: 14,
+    },
+}
