807e38e0f7e01ba77e90ee558c89cd9a9c304bb1
Dicka Lezama <dj_lezama@hotmail.com>
2025-03-08 20:32:51 -0600

    Changed typography to match fonts required by NavigationTheme

--- src/config/theme/typography.ts
+++ src/config/theme/typography.ts
@@ -1,8 +1,4 @@
 export const typography = {
-    fontFamily: {
-        medium: "Inter-Medium",
-        bold: "Inter-Bold",
-    },
     fontSize: {
         xl: 64, // i.e "Bienvenido a DocWallet" en la página de inicio
         lg: 60, // i.e los títulos de páginas "Carpetas", "Documentos de viaje", etc.
@@ -11,6 +7,7 @@ export const typography = {
         sm: 15, // i.e "Ya tengo una cuenta" en la página de registro
         xm: 12,
     },
+
     lineHeight: {
         xl: 60,
         lg: 60,
@@ -18,5 +15,13 @@ export const typography = {
         base: 23,
         sm: 18,
         xm: 14,
+    },
+
+    // Only medium and bold used in the Figma
+    fonts: {
+        regular: { fontFamily: "Inter-Regular", fontWeight: "400" as const },
+        medium: { fontFamily: "Inter-Medium", fontWeight: "500" as const },
+        bold: { fontFamily: "Inter-Bold", fontWeight: "700" as const },
+        heavy: { fontFamily: "Inter-Heavy", fontWeight: "900" as const },
     },
 }
